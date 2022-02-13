ce68557b4059d8715e51ceec00177b8f4163726a
honeyonsys <honeyonsys@gmail.com>
2022-02-14 00:24:28 +0530

    updating cart product on add to cart button click

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+function refreshCartCount(count) {
+    try {
+        document.querySelector('#cartCount').innerText = count;
+    } 
+    catch(err) {
+        console.log("Error: " + err.message);
+    }
+}
 // Loading sidebar from sidebar.html
-var currentUrl = window.location.href;
-console.log(currentUrl);
 var request = new XMLHttpRequest();
 request.open('GET', 'navigation.html', true);
 request.onload = function() {
 if (request.status >= 200 && request.status < 400) {
-    var resp = request.responseText;
-    document.querySelector('.navigationBar').innerHTML = resp;
+        var resp = request.responseText;
+        document.querySelector('.navigationBar').innerHTML = resp;
+        // updating cart count on top navigation
+        let cartItems = localStorage.getItem('Grocery-Cart');
+        let cartJson = Array.from(JSON.parse(cartItems));
+        refreshCartCount(cartJson.length);
     }
 };
 request.send();
